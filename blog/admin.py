122bcd3218fa4813e13fb1bef9086b97ab974a3a
ironman <2012joe@gmail.com>
2020-06-20 13:20:26 +0800

    Customizing the way that Post Model displayed on backends.

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    """
+    raw_id_fields: The author field is now displayed with a lookup widget that can scale much better than a drop-down select input 
+    when you have thousands of users.
+    """
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
